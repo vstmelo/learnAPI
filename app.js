ad33b846e87328ec80ba13c78b35a444d7e7dd26
Vmeelo <vinicius.melo@dcomp.ufs.br>
2021-11-30 23:58:47 -0300

    feat: new routes.

--- app.js
+++ app.js
@@ -2,33 +2,29 @@ const express = require('express');
 
 const app = express();
 
+const routes = require('./routes/routes');
+
+
+app.use(routes);
+
+app.use(express.json());
 
 const products = [
     {
-        nome: "leite",
+        name: "leite",
+        price: 2,
+        id: 0,
+    },
+    {
+        name: "arroz",
+        price: 2,
         id: 1,
     },
     {
-        nome: "arroz",
+        name: "feijao",
+        price: 2,
         id: 2,
     },
-    {
-        nome: "feijao",
-        id: 3,
-    },
 ];
-
-//An rout to get all products
-
-app.get('/products', (req, res) => {
-    res.json(products);
-});
-
-//Get a specific object from array products
-app.get('/products/1', (req, res) => {
-    // get the product by position
-    const product = products[1]; 
-    res.json(product);
-});
 
 app.listen(8000);
